0380017f6045f24f4063a84b782ab957e773223f
Ryan Alli <ryanalli77@gmail.com>
2024-01-30 17:11:55 -0500

    white to blue gradietn

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include"color.h"
+#include"ray.h"
 #include"vec3.h"
 
 #include<iostream>
+color rayColor(const ray& r)
+{
+  vec3 unitDirection = unitVector(r.direction());
+  auto a = 0.5*(unitDirection.y() + 1.0);
+  return (1-a) * color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
+}
 
 int main()
 {
-  int imageWidth = 256;
-  int imageHeight = 256;
+  auto aspectRatio = 16.0 / 9.0;
+  int imageWidth = 400;
+  //calculate height based on widht and aspect ratio
+  int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+  // if image height is less than 1
+  imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+  
+  //Camera stuff
+  auto focalLength = 1.0;
+  auto viewportHeight = 2.0;
+  auto viewportWidth = viewportHeight * (static_cast<double> (imageWidth)/ imageHeight);
+  auto cameraCenter = point3(0,0,0);
+
+  //calc vectors across the horizontal and vertical viewport edges
+  auto viewportU = vec3(viewportWidth, 0, 0);
+  auto viewportV = vec3(0, -viewportWidth, 0);
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  auto pixelDeltaU = viewportU / imageWidth;
+  auto pixelDeltaV = viewportV / imageHeight;
+  // Calculate the location of the upper left pixel.
+  auto viewportUpperLeft = cameraCenter - vec3(0,0,focalLength) - viewportU/2 - viewportV/2;
+
+  auto pixel00Location = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+
+  //Render
+
   std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 
   for(int j = 0; j < imageHeight; j++)
@@ -14,7 +45,13 @@ int main()
     std::clog << "\rScanlines: " << (imageHeight - j) << ' ' << std::flush;
     for(int i = 0; i < imageWidth; i++)
     {
-      auto pixelColor = color(double(i)/(imageWidth - 1), double(j)/(imageWidth - 1), 0);
+      auto pixelCenter = pixel00Location + (i * pixelDeltaU) + (j * pixelDeltaV);
+      auto rayDirection = pixelCenter - cameraCenter;
+      ray r(cameraCenter, rayDirection);
+
+      //assigns pixel to color based off ray
+      color pixelColor = rayColor(r);
+      
       writeColor(std::cout, pixelColor);
     }
   }
